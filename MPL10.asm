0106273c998307374a6923360b5c9287779b0f00
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2025-05-17 10:46:48 +0530

    MPL10.asm

--- MPL10.asm
+++ MPL10.asm
@@ -1,3 +1,4 @@
+;Write X86/64 ALP to perform multiplication of two 8-bit hexadecimal numbers. Use successive addition and add and shift method. (use of 64-bit registers is expected).
 %macro IO 4
     mov rax, %1
     mov rdi, %2
