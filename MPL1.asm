f468664cd4acd57ef3caa879ccc7c453d6eb9986
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2025-05-17 10:41:19 +0530

    MPL1.asm

--- MPL1.asm
+++ MPL1.asm
@@ -1,3 +1,5 @@
+;Write an X86/64 ALP to accept five 64 bit Hexadecimal numbers from user and store them in an array and display the accepted numbers.
+
 section .data
 msg1 db 10,13,"Enter 5 64 bit numbers", 0
 len1 equ $-msg1
