0b5f84d25f15181be0f7f4f0ed12c066eb2d975b
Disha Gaikwad <dishagaikwad10a06@gmail.com>
2025-05-17 10:45:43 +0530

    MPL8.asm

--- MPL8.asm
+++ MPL8.asm
@@ -1,3 +1,5 @@
+;Write X86/64 ALP to perform non-overlapped block transfer without string specific instructions. Block containing data can be defined in the data segment.
+
 %macro IO 4
     mov rax, %1
     mov rdi, %2
